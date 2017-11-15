404168593cf18d681b2ed25bb2784db4709acb8b
Jen <error@error.media>
2017-11-14 21:29:13 -0800

    globe

--- main3.js
+++ main3.js
@@ -5,131 +5,149 @@
     preserveDrawingBuffer: true
   })
   var scene = new THREE.Scene()
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 300)
-  camera.position.z = 130
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 100)
+  camera.position.z = 50
+
   var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1)
+
   var orbit = new THREE.OrbitControls(camera, renderer.domElement)
-  orbit.enableZoom = true
+  orbit.enableZoom = false
   var bounce = false
 
-  var p2 = 0.405; // pressure
-  var p1 = 0.0001; // pressure
-  var v2 = 0.1010; // speed
-  var v1 = 0.101; // speed
-  var h1 = 12.400// height
+  var p2 = 0.9405; // pressure
+  var p1 = 0.3001; // pressure
+  var v2 = 0.3010; // speed
+  var v1 = 0.501; // speed
+  var h1 = 0.4012// height
   var h2 = 0.101; // height
-  var g = 9.8 // m/sec
+  var g = -1.8 // m/sec
   var group = []
   var p = 0.0
   var pos = 1.001
   var idx = 0
 
   function zoom() {
-    for (var i = 0; i < group.length; i++) {
-      group[i].circle.scale.x += 0.1
-      group[i].material.opacity -= 0.01
 
+    if (group.circle.scale.x >= 2.0) {
+      group.circle.scale.x = 1.0
+      group.circle.scale.y = 1.0
+      group.circle.scale.z = 1.0
     }
+
+    group.circle.scale.x += 0.1
+    group.circle.scale.y += 0.1
+    group.circle.scale.z += 0.1
   }
 
-  window.onclick = function (ev) {
+  window.onclick = window.onmouseover = function (ev) {
     zoom()
   }
 
   function init() {
+    renderer.preserveDrawingBuffer = true
     renderer.autoClearColor = false
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setClearColor(0x000000, 1)
+    renderer.setClearColor( 0xffffff, 0)
     container.appendChild(renderer.domElement)
 
     scene.add(camera)
     scene.add(light)
 
-    for (var i = 0; i < 10; i++) {
-      var material = new THREE.MeshLambertMaterial({
-        color: '#00e1d9'
-      })
-      var geometry = new THREE.SphereGeometry(40, 120, 40)
+    var currX = 15
+    var currY = 0
+    var currZ = -Math.sin(90)
 
-      // geometry.vertices.shift()
-      var circle = new THREE.Mesh(geometry, material)
+    var material = new THREE.MeshLambertMaterial({
+      color: '#00e1d9',
+      wireframe: true
+    })
+    var geometry = new THREE.SphereGeometry(20, 120, 100)
 
-      material.transparent = true
-      material.opacity = 0.01
+    var circle = new THREE.Mesh(geometry, material)
 
-      p1 += 0.771
-      p2 += 0.001
+    material.transparent = true
+    material.opacity = 0.007
 
-      pos += 0.101
-      v1 += Math.sin(v1) * Math.random()
-      v2 += Math.sin(v2) * Math.random()
+    currX += 2.01
+    currY += 0.01
 
-      circle.position.x += p1
-      circle.position.z += p2
-      circle.position.y += p
+    circle.rotation.z += currZ
+    circle.rotation.x += currX
 
-      group.push({
-        p1: p1,
-        p2: p2,
-        p: p,
-        pos: pos,
-        v1: v1,
-        v2: v2,
-        material: material,
-        circle: circle
-      })
+    p1 += 0.771
+    p2 += 0.301
 
-      scene.add(circle)
+    pos += 0.101
+    v1 = Math.sin(v1) * Math.random()
+    v2 = Math.sin(v2) * Math.random()
+
+    circle.position.x += 1.0
+    circle.rotation.y += 1.0
+
+    group = {
+      p1: p1,
+      p2: p2,
+      p: p,
+      pos: pos,
+      v1: v1,
+      v2: v2,
+      material: material,
+      circle: circle
     }
+
+    scene.add(circle)
   }
 
   var count = 0
 
-  function bernP2 (idx) {
-    group[idx].p2 = group[idx].p1 + (0.5 * group[idx].p * Math.pow(group[idx].v1, 2)) + (group[idx].p * g * h1) - (0.5 * group[idx].p * Math.pow(group[idx].v2, 2)) - (group[idx].p * g * h2)
+  function bernP2 () {
+    group.p2 = group.p1 + (0.5 * group.p * Math.pow(group.v1, 2)) + (group.p * g * h1) - (0.5 * group.p * Math.pow(group.v2, 2)) - (group.p * g * h2)
 
     if (bounce) {
-      group[idx].material.opacity -= 0.01
-      group[idx].p1 -= 0.2011
-      group[idx].p2 -= 0.3008
-      group[idx].p  -= 0.901
-      group[idx].circle.rotation.z += Math.cos(group[idx].p2) * group[idx].p2
-      group[idx].circle.rotation.y += Math.cos(group[idx].p2) * group[idx].pos
-      group[idx].circle.position.y += Math.cos(group[idx].p2) * group[idx].p1
-      group[idx].circle.position.z += Math.cos(group[idx].p2) * group[idx].p
-      group[idx].material.color.set('#e1006f')
+      //renderer.clear()
+      //renderer.preserveDrawingBuffer = false
+      //group[idx].material.opacity -= 0.01
+
+      group.p1 -= 0.2011
+      group.p2 -= 0.0501
+      group.p  -= 0.901
+
+      group.circle.rotation.x -= 1.01
+      group.circle.rotation.y -= 1
+      group.circle.position.y -= 0.002
+      group.circle.position.x -= 0.002
+
+      group.material.color.set('rgb(20, 180, 190)')
 
     } else {
-      group[idx].material.opacity += 0.01
-      group[idx].p1 += 0.2011
-      group[idx].p2 += 0.3008
-      group[idx].p += 0.901
-      group[idx].circle.rotation.z += Math.sin(group[idx].p2) * group[idx].p2
-      group[idx].circle.rotation.y += Math.sin(group[idx].p2) * group[idx].pos
-      group[idx].circle.position.y += Math.sin(group[idx].p2) * group[idx].p1
-      group[idx].circle.position.z += Math.sin(group[idx].p2) * group[idx].p
-      group[idx].material.color.set('#00f1d9')
+
+      group.p1 += 10.2011
+      group.p2 += 0.0501
+      group.p += 0.901
+
+      group.circle.rotation.x += 1.01
+      group.circle.rotation.y += 1
+      group.circle.position.y += 0.002
+      group.circle.position.x += 0.002
+
+      group.material.color.set('rgb(230, 10, 150)')
     }
 
-    if (group[idx].p <= 0.0 || group[idx].p > window.innerWidth) {
-      group[idx].p = Math.sin(145) * group[idx].p
-    }
+   // group[idx].circle.rotation.z += Math.sin(group[idx].p1) * 2 * Math.PI
+    group.circle.rotation.z += Math.sin(45) * 1.03
+
   }
 
   function render() {
     count++
-    if (count % 500 === 0) {
+    if (count % 200 === 0) {
       bounce = !bounce
     }
 
-    bernP2(idx)
-
-    idx++
-
-    if (idx >= group.length) {
-      idx = 0
-    }
+    setTimeout(function () {
+      bernP2()
+    }, 500)
 
     renderer.render(scene, camera)
 
